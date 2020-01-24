36b9d154e86cedfe5abe70885ebdb91545a017ac
Student <student@imac-18.local>
2020-01-24 09:38:35 +0100

    initial commit

---
+++ IOSBackgroundTasks/IOSBackgroundTasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSBackgroundTasks
+//
+//  Created by Student on 24/01/2020.
+//  Copyright © 2020 ms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
