6a36c16fa131a1694e9efec737e1d0e52451f390
Student <student@imac-18.local>
2020-01-24 11:03:59 +0100

    Load image

--- IOSBackgroundTasks/IOSBackgroundTasks/ViewController.swift
+++ IOSBackgroundTasks/IOSBackgroundTasks/ViewController.swift
@@ -8,13 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    let URLS: [URL] = [ URL(string: "https://tinyjpg.com/images/social/website.jpg")!
+                ]
+    var imageTaskList: [ImageDownloadTask] = []
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.text = ""
     }
 
+    @IBAction func startButton(_ sender: Any) {
+        let urlSession: URLSession = {
+            let config = URLSessionConfiguration.background(withIdentifier: "MySession")
+            config.isDiscretionary = true
+            config.sessionSendsLaunchEvents = true
+            return URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        }()
+        
+        for url in URLS {
+            let backgroundTask = urlSession.downloadTask(with: url)
+            let img = ImageDownloadTask(id: backgroundTask.taskIdentifier, fileName: url.lastPathComponent, progress: 0.00)
+            imageTaskList.append(img)
+            backgroundTask.resume()
+            logToTextView(message: "start downloading image: \(img.id)")
+        }
+    }
 
+    @IBAction func resetButton(_ sender: Any) {
+        textView.text = ""
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        logToTextView(message: "Downloading finished")
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let progress: Double = round(Double(totalBytesWritten) / Double(totalBytesExpectedToWrite) * 100.00)
+        let id = downloadTask.taskIdentifier
+        if let foundObj = imageTaskList.first(where: { $0.id == id}) {
+            foundObj.progress = progress
+            logToTextView(message: "Printing \(foundObj.id) image with progress: \(foundObj.progress) %% ")
+        }
+        
+    }
+    
+    func logToTextView(message: String){
+        DispatchQueue.main.async {
+            self.textView.text += message + "\n"
+        }
+    }
+    
 }
+
+class ImageDownloadTask {
+    var id: Int
+    var fileName: String
+    var progress: Double
+    
+    init(id: Int, fileName: String, progress: Double) {
+        self.id = id
+        self.fileName = fileName
+        self.progress = progress
+    }
+    
+    func setProgress(progress: Double) {
+        
+    }
+}
+
+
+
 
